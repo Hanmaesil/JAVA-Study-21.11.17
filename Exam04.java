df7fdd65395b9ca00a587041e9fef8253b443de5
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-17 12:48:29 +0900

    문자열 연산

---
+++ Exam04.java
@@ -0,0 +1,25 @@
+
+public class Exam04 {
+
+	public static void main(String[] args) {
+		
+		//문자열 연산
+		String value = "나는 사과";
+		System.out.println(value + "입니다." + 8 + 9); //문자열이 우세하기 때문에 정수형을 더해도 문자열로 출력된다.(가로로 구획을 치면 가로 먼저 실행)
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
