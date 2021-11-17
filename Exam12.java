bb39373e275a09e617475136d534d16b8f03b51f
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-17 16:00:58 +0900

    삼항연산자

---
+++ Exam12.java
@@ -0,0 +1,23 @@
+import java.util.Scanner;
+
+public class Exam12 {
+
+	public static void main(String[] args) {
+		//삼항연산자 : 조건식 ? 실행1 : 실행2 >>조건식이 true 일때 실행1이 출력되고 false이면 실행2가 출력된다.
+		
+		//삼항연산자 안에 또다른 삼항연산자도 가능하다
+		//System.out.println(i == j ? (a >= b : "같다" : "다르다") : "다르다");
+		
+		int i = 3;
+		int j = 10;
+		
+		System.out.println(i == j ? "같다" : "다르다");
+		 
+		
+		
+		
+		
+		
+	}
+
+}
