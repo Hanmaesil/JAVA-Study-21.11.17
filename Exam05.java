e24a583e6ff5b6e9649b05e2479255e227069e12
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-17 12:48:59 +0900

    실습
    1 ~ 4 까지의 내용 실습

---
+++ Exam05.java
@@ -0,0 +1,28 @@
+
+public class Exam05 {
+
+	public static void main(String[] args) {
+		
+		//실습
+		
+		int num1 = 7;
+		int num2 = 3;
+		
+		System.out.println("더하기 결과 : " + (num1 + num2));
+		System.out.println("빼기 결과 : " + (num1 - num2));
+		System.out.println("곱하기 결과 : " + num1 * num2);
+		System.out.println("나누기 결과 : " +((double)num1 / num2));
+		System.out.println("나누기 결과 : " +(num1 * 1.0 / num2));
+		
+		double num3 = num2;
+		System.out.println(num1 / num3);
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
