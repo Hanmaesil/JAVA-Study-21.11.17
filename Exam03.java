bb6ca2120f25ad175ecd01227f2b4ce653a4ed11
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-17 12:48:04 +0900

    정수와 실수 연산

---
+++ Exam03.java
@@ -0,0 +1,39 @@
+
+public class Exam03 {
+
+	public static void main(String[] args) {
+		
+		int num1 = 7;
+		int num2 = 3;
+		double num3 = 3.0; // 정수와 실수를 연산했을 때 실수를 더 큰값으로 인식해서 실수로 출력된다.
+		
+		System.out.println(num1 + num2);
+		System.out.println(num1 - num2);
+		System.out.println(num1 * num2);
+		System.out.println(num1 / num2);
+		System.out.println(num1 % num2);
+		System.out.println(num1 / num3);
+		System.out.println(num1 % num3); //나머지 연산자는 처음만 나온다.
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+		
+	}
+
+}
