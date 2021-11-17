83c1ecd93c537e55354346cc29afba5491f4ba42
Hanmaesil <93114691+Hanmaesil@users.noreply.github.com>
2021-11-17 12:47:02 +0900

    문자열 변수, 상수

---
+++ Exam01.java
@@ -0,0 +1,20 @@
+
+public class Exam01 {
+//main + ctrl + space로 메인 만들수 있음.
+	public static void main(String[] args) {
+	
+		//문자열 변수
+		String name = "한국인";
+		System.out.println(name);
+		
+		//상수 : 변하지 않는 값을 가진 것.
+		final int age = 29;
+		
+		
+		
+		
+		
+	}
+}
+
+
